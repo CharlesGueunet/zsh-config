ff43ec8e30c20f7fc670ca7d1376d99286164711
Charles Gueunet <charles.gueunet@gmail.com>
2018-04-25 13:54:39 +0200

    perf(clone): limit the history size when cloning

--- plugins_list.zsh
+++ plugins_list.zsh
@@ -1,18 +1,18 @@
-# prezto
-zplug  "modules/environment", from:prezto
-zplug  "modules/editor", from:prezto, on:"modules/environment"
-zplug  "modules/history", from:prezto, on:"modules/environment"
-zplug  "modules/directory", from:prezto, on:"modules/environment"
-zplug  "modules/utility", from:prezto, on:"modules/environment"
-zplug  "modules/spectrum", from:prezto, on:"modules/environment"
-zplug  "modules/ssh", from:prezto, on:"modules/environment"
-zplug  "modules/rsync", from:prezto, on:"modules/environment"
-zplug  "modules/archive", from:prezto, on:"modules/environment"
-zplug  "modules/tmux", from:prezto, on:"modules/environment"
-zplug  "modules/autosuggestions", from:prezto, on:"modules/environment"
-zplug  "modules/history-substring-search", from:prezto, on:"modules/environment"
-zplug  "modules/prompt", from:prezto, on:"modules/environment"
+# prezto, depth:1
+zplug  "modules/environment", from:prezto, depth:1
+zplug  "modules/editor", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/history", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/directory", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/utility", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/spectrum", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/ssh", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/rsync", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/archive", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/tmux", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/autosuggestions", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/history-substring-search", from:prezto, depth:1, on:"modules/environment"
+zplug  "modules/prompt", from:prezto, depth:1, on:"modules/environment"
 
 # other
-zplug "zsh-users/zsh-syntax-highlighting", defer:1
+zplug "zsh-users/zsh-syntax-highlighting", depth:1, defer:1
 zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf
