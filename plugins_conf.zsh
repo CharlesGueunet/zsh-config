72569da98fca72d2087834934d7b06298a464db1
Charles Gueunet <charles.gueunet@gmail.com>
2018-04-28 15:33:39 +0200

    chore(plugin): clean config file and add more verif

--- plugins_conf.zsh
+++ plugins_conf.zsh
@@ -1,43 +1,32 @@
-#
-# Editor
-#
+if zplug check 'modules/editor'; then
+   # Set the key mapping style to 'emacs' or 'vi'. : remove tab completion
+   zstyle ':prezto:module:editor' key-bindings 'vi'
 
-# Set the key mapping style to 'emacs' or 'vi'. : remove tab completion
-zstyle ':prezto:module:editor' key-bindings 'vi'
+   # Auto convert .... to ../..
+   zstyle ':prezto:module:editor' dot-expansion 'yes'
+fi
 
-# Auto convert .... to ../..
-zstyle ':prezto:module:editor' dot-expansion 'yes'
+if zplug check 'modules/ssh'; then
+   # Set the SSH identities to load into the agent.
+   zstyle ':prezto:module:ssh:load' identities 'id_rsa' 'id_rsa2' 'id_github'
+fi
+
+if zplug check 'modules/autosuggestions'; then
+   zstyle ':prezto:module:autosuggestions' color 'yes'
+   zstyle ':prezto:module:autosuggestions:color' found 'fg=cyan'
+fi
 
 
-#
-# SSH
-#
+if zplug check 'modules/history-substring-search'; then
+   # Set the query found color.
+   zstyle ':prezto:module:history-substring-search:color' found 'blue'
+   # Set the query not found color.
+   zstyle ':prezto:module:history-substring-search:color' not-found 'red'
+fi
 
-# Set the SSH identities to load into the agent.
-zstyle ':prezto:module:ssh:load' identities 'id_rsa' 'id_rsa2' 'id_github'
-
-#
-# Auto-Suggestions
-#
-
-zstyle ':prezto:module:autosuggestions' color 'yes'
-zstyle ':prezto:module:autosuggestions:color' found 'fg=cyan'
-
-#
-# History Substring Search
-#
-
-# Set the query found color.
-zstyle ':prezto:module:history-substring-search:color' found 'blue'
-
-# Set the query not found color.
-zstyle ':prezto:module:history-substring-search:color' not-found 'red'
-
-#
-# Prompt
-#
-
-# Set the prompt theme to load.
-# Setting it to 'random' loads a random theme.
-# Auto set to 'off' on dumb terminals.
-zstyle ':prezto:module:prompt' theme 'grml'
+if zplug check 'modules/prompt'; then
+   # Set the prompt theme to load.
+   # Setting it to 'random' loads a random theme.
+   # Auto set to 'off' on dumb terminals.
+   zstyle ':prezto:module:prompt' theme 'grml'
+fi
