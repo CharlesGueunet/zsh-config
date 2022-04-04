63531a4a13141b67c5c9f857f7a432aceef39d5e
Charles Gueunet <charles.gueunet@gmail.com>
2022-04-04 13:51:49 +0200

    fzf ignore

--- plugins_conf.zsh
+++ plugins_conf.zsh
@@ -5,6 +5,6 @@ bindkey -M viins '^F' vi-forward-word
 
 # FZF
 export FZF_DEFAULT_OPTS='--height 40% --border --preview "less -$LINES {}"'
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore-dir={.git,.ccls-cache,build,buildD,buildR} -g ""'
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore-dir={.git,.ccls-cache,.cache,build} -g ""'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
